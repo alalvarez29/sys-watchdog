2809b68bca578d8ae6e2fafbe1b09a8bf175414d
Anderson Álvarez <al.alvarez439@gmail.com>
2024-10-27 16:45:14 -0500

    RAM usage functionality

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
 use sysinfo::System;
+use std::io::{self, Write};
 
 fn main() {
     //Initialize system
     let mut sys = System::new();
 
-    println!("Monitoring CPU usage...");
+    print!("\x1B[2J\x1B[1;1H");
+    println!("Monitoring system...");
 
     loop {
         //Refresh CPU info
         sys.refresh_cpu();
+        //Refresh memory info
+        sys.refresh_memory();
 
         //Get CPU global use information
         let cpu_usage = sys.global_cpu_info().cpu_usage();
+        let total_ram = sys.total_memory() / 1024 / 1024;
+        let used_ram = (sys.total_memory() - sys.available_memory()) / 1024         /1024;
+        let ram_usage = (used_ram as f32 / total_ram as f32) * 100.0;
+
+        print!("\r\x1B[K");
         
         //Print the info
-        println!("CPU: {:.1}%", cpu_usage);
+        print!("CPU: {:.1}% | RAM: {:.1}% ({} MB / {} MB)", 
+               cpu_usage, ram_usage, used_ram, total_ram);
+
+        io::stdout().flush().unwrap();
 
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
