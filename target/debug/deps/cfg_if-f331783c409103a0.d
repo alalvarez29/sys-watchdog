da3c1742099b45fa932630cc67728ae576cbe385
Anderson Álvarez <al.alvarez439@gmail.com>
2024-11-01 11:39:36 -0500

    target folder deleted

--- target/debug/deps/cfg_if-f331783c409103a0.d
+++
@@ -1,7 +0,0 @@
-/home/uqbarcitizen/rustprojects/sys-watchdog/target/debug/deps/libcfg_if-f331783c409103a0.rmeta: /home/uqbarcitizen/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/uqbarcitizen/rustprojects/sys-watchdog/target/debug/deps/libcfg_if-f331783c409103a0.rlib: /home/uqbarcitizen/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/uqbarcitizen/rustprojects/sys-watchdog/target/debug/deps/cfg_if-f331783c409103a0.d: /home/uqbarcitizen/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs
-
-/home/uqbarcitizen/.cargo/registry/src/index.crates.io-6f17d22bba15001f/cfg-if-1.0.0/src/lib.rs:
--- target/debug/sys-watchdog.d
+++
@@ -1,1 +0,0 @@
-/home/uqbarcitizen/rustprojects/sys-watchdog/target/debug/sys-watchdog: /home/uqbarcitizen/rustprojects/sys-watchdog/src/main.rs
